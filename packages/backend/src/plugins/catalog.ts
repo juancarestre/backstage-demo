cde9a332cd2ce8618653d5a51e980d8ac45080cf
juan restrepo <jcrestrepobedoya@gmail.com>
2023-10-27 15:20:53 -0500

    backstage demo

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,12 +2,32 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { GithubOrgEntityProvider, defaultUserTransformer } from '@backstage/plugin-catalog-backend-module-github';
+
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addEntityProvider(
+    GithubOrgEntityProvider.fromConfig(env.config, {
+      id: 'production',
+      orgUrl: 'https://github.com/hesitationgames',
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 1 },
+        timeout: { minutes: 15 },
+      }),
+      userTransformer: async (user, ctx) => {
+        const entity = await defaultUserTransformer(user, ctx);
+        if (entity && user.organizationVerifiedDomainEmails?.length) {
+          entity.spec.profile!.email = user.organizationVerifiedDomainEmails[0];
+        }
+        return entity;
+      },
+    }),
+  );
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
