901a6b0479be8493e98a633265580b4c4e609166
juan restrepo <jcrestrepobedoya@gmail.com>
2023-11-01 11:34:00 -0500

    nice

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,7 +1,33 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import { createBuiltinActions, createRouter, TemplateAction } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
+import { createHttpBackstageAction } from '@roadiehq/scaffolder-backend-module-http-request'
+import { Config } from '@backstage/config';
+import { ScmIntegrations } from '@backstage/integration';
+import { UrlReader } from '@backstage/backend-common';
+import { DiscoveryApi } from '@backstage/plugin-permission-common';
+
+export const createActions = (options: {
+  reader: UrlReader;
+  integrations: ScmIntegrations;
+  config: Config;
+  catalogClient: CatalogClient;
+  discoveryApi: DiscoveryApi
+}): TemplateAction<any>[] => {
+  const { reader, integrations, config, catalogClient } = options;
+  const defaultActions = createBuiltinActions({
+    reader,
+    integrations,
+    catalogClient,
+    config,
+  });
+
+  return [
+    createHttpBackstageAction({ discovery: options.discoveryApi  }),
+    ...defaultActions,
+  ];
+};
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -16,7 +42,13 @@ export default async function createPlugin(
     database: env.database,
     reader: env.reader,
     catalogClient,
-    identity: env.identity,
     permissions: env.permissions,
+    actions: createActions({
+      reader: env.reader,
+      integrations: ScmIntegrations.fromConfig(env.config),
+      config: env.config,
+      catalogClient: catalogClient,
+      discoveryApi: env.discovery,
+    }),
   });
 }
